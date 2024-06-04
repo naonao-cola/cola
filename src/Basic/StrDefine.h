fabb19eeb416b45d5f3f01a466efa0a35e60492c
naonao <1319144981@qq.com>
2024-06-04 16:11:03 +0800

    add basic

---
+++ src/Basic/StrDefine.h
@@ -0,0 +1,26 @@
+﻿/**
+ * @FilePath     : /cola/src/Basic/StrDefine.h
+ * @Description  :
+ * @Author       : naonao 1319144981@qq.com
+ * @Version      : 0.0.1
+ * @LastEditors  : naonao 1319144981@qq.com
+ * @LastEditTime : 2024-06-04 15:53:46
+ * @Copyright    : G AUTOMOBILE RESEARCH INSTITUTE CO.,LTD Copyright (c) 2024.
+**/
+
+#ifndef NAO_STRDEFINE_H
+#define NAO_STRDEFINE_H
+
+#include "BasicDefine.h"
+
+NAO_NAMESPACE_BEGIN
+
+static const char* NAO_EMPTY = "";
+static const char* NAO_DEFAULT = "default";
+static const char* NAO_UNKNOWN = "unknown";
+static const char* NAO_FUNCTION_NO_SUPPORT = "function no support";
+static const char* NAO_INPUT_IS_NULL = "input is nullptr";
+
+NAO_NAMESPACE_END
+
+#endif //NAO_STRDEFINE_H
