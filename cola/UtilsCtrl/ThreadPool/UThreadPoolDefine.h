32dba0fed664342376073ba205fa6bec34e3430e
naonao <1319144981@qq.com>
2024-09-05 11:13:16 +0800

    fix dag 0820 01

--- cola/UtilsCtrl/ThreadPool/UThreadPoolDefine.h
+++ cola/UtilsCtrl/ThreadPool/UThreadPoolDefine.h
@@ -4,7 +4,7 @@
  * @Author       : naonao
  * @Version      : 0.0.1
  * @LastEditors  : naonao
- * @LastEditTime : 2024-08-12 13:37:23
+ * @LastEditTime : 2024-09-05 11:10:22
  **/
 
 #ifndef NAO_UTHREADPOOLDEFINE_H
@@ -41,7 +41,7 @@ static const NMSec  NAO_MAX_BLOCK_TTL              = 1999999999;   // 最大阻�
 static const NUInt  NAO_DEFAULT_RINGBUFFER_SIZE    = 64;           // 默认环形队列的大小
 static const NIndex NAO_MAIN_THREAD_ID             = -1;           // 启动线程id标识（非上述主线程）
 static const NIndex NAO_SECONDARY_THREAD_COMMON_ID = -2;           // 辅助线程统一id标识
-static const NInt   NAO_DEFAULT_PRIORITY           = 0;            // 默认优先级
+
 
 
 static const NInt NAO_DEFAULT_TASK_STRATEGY   = -1;     // 默认线程调度策略
@@ -53,14 +53,14 @@ static const NInt NAO_LONG_TIME_TASK_STRATEGY = -101;   // 长时间任务调度
  */
 static const NInt  NAO_DEFAULT_THREAD_SIZE           = 8;                        // 默认开启主线程个数
 static const NInt  NAO_SECONDARY_THREAD_SIZE         = 0;                        // 默认开启辅助线程个数
-static const NInt  NAO_MAX_THREAD_SIZE               = 16;                       // 最大线程个数
-static const NInt  NAO_MAX_TASK_STEAL_RANGE          = 2;                        // 盗取机制相邻范围
+static const NInt  NAO_MAX_THREAD_SIZE               = 8;                       // 最大线程个数
+static const NInt  NAO_MAX_TASK_STEAL_RANGE          = 7;                        // 盗取机制相邻范围
 static const NBool NAO_BATCH_TASK_ENABLE             = false;                    // 是否开启批量任务功能
 static const NInt  NAO_MAX_LOCAL_BATCH_SIZE          = 2;                        // 批量执行本地任务最大值
 static const NInt  NAO_MAX_POOL_BATCH_SIZE           = 2;                        // 批量执行通用任务最大值
 static const NInt  NAO_MAX_STEAL_BATCH_SIZE          = 2;                        // 批量盗取任务最大值
 static const NInt  NAO_PRIMARY_THREAD_BUSY_EPOCH     = 10;                       // 主线程进入wait状态的轮数，数值越大，理论性能越高，但空转可能性也越大
-static const NMSec NAO_PRIMARY_THREAD_EMPTY_INTERVAL = 50;                       // 主线程进入休眠状态的默认时间
+static const NMSec NAO_PRIMARY_THREAD_EMPTY_INTERVAL = 10;                       // 主线程进入休眠状态的默认时间
 static const NSec  NAO_SECONDARY_THREAD_TTL          = 10;                       // 辅助线程ttl，单位为s
 static const NBool NAO_MONITOR_ENABLE                = false;                    // 是否开启监控程序
 static const NSec  NAO_MONITOR_SPAN                  = 5;                        // 监控线程执行间隔，单位为s
